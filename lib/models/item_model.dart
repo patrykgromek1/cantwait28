10664f5df832dc7243bc9e5bde6c0a1de9c63c09
Patryk Gromek <patryk.gromekk@gmail.com>
2023-03-18 19:05:04 +0100

    Create item model

---
+++ lib/models/item_model.dart
@@ -0,0 +1,11 @@
+class ItemModel {
+  ItemModel({
+    required this.title,
+    required this.imageURL,
+    required this.releaseDate,
+  });
+
+  final String title;
+  final String imageURL;
+  final DateTime releaseDate;
+}
